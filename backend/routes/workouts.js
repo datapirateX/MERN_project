1bb212771ee694a8eb619c134628a63982929455
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-12-26 10:17:00 +0600

    working on database

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,15 +1,38 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
-router.get('./', (req, res) => {
-    res.json({mssg: 'GET all workout'})
+// GET all workouts
+router.get('/', (req, res) => {
+  res.json({mssg: 'GET all workouts'})
 })
 
-router.get('./:id', (res, req) => {
-    res.json({mssg: 'GET a single workout'})
+// GET a single workout
+router.get('/:id', (req, res) => {
+  res.json({mssg: 'GET a single workout'})
 })
 
-router.post('/')
+// POST a new workout
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    try {
+        const workout = await Workout.create({title, load, reps})
+
+    } catch (error){
+
+    }
+  res.json({mssg: 'POST a new workout'})
+})
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'DELETE a workout'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'UPDATE a workout'})
+})
 
 module.exports = router
